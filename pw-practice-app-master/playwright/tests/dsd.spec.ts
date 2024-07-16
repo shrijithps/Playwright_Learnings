5daef995f2ca656da5c30d6da2fe31e62d618957
P S Shrijith <shrijithps89@gmail.com>
2024-07-16 19:52:16 +0530

    Delete pw-practice-app-master/playwright/tests/dsd.spec.ts

--- pw-practice-app-master/playwright/tests/dsd.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test('proview login', async({page}) => {
-    await page.goto('https://admin.talview.org/login?redirect_url=https%253A%252F%252Fproview-console.talview.org%252Flogin&username=sandeep.hm+69@talview.com');
-    await page.locator("[type='password']").fill("Talview@123")
-    await page.locator('button', {hasText:'Sign In'}).click();
-    await page.waitForURL("https://proview-console.talview.org/login?");
-    await page.goto('https://proview-console.talview.org/')
-})
