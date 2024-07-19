2741ab4aaee213b993a521444b39a9e96518e2ff
P S Shrijith <shrijithps89@gmail.com>
2024-07-19 10:17:12 +0530

    handling dropdowns

--- pw-practice-app-master/playwright/tests/redbus.spec.ts
+++ pw-practice-app-master/playwright/tests/redbus.spec.ts
@@ -1,16 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test("Selecting dropdown", async({page}) => {
+test("Selecting dropdown", async ({ page }) => {
     await page.goto("https://redbus.in");
-    // await page.locator('label',{hasText:"From"}).click();
-    await page.locator('#src').click()
-    await page.locator('#src').fill("Bengaluru");
-    const text = await page.locator(".sc-dnqmqq dZhbJF").locator('text').allInnerTexts();
-    for(const a of await text){
-        console.log(a);
-        if (a == "Bengaluru"){
-            page.locator('text', {hasText:a}).click()
-        }
 
+    // Click on the source input field
+    await page.locator('#src').click();
+
+    // Type "Bengaluru" into the input field
+    await page.locator('#src').type("Bengaluru",{delay:1000});
+    // await page.keyboard.type('World', { delay: 100 }); // Types slower, like a user
+
+
+    await page.waitForTimeout(5000)
+    // Wait for the dropdown to appear
+    await page.waitForSelector('.sc-dnqmqq.dZhbJF'); // Adjust selector as per the actual dropdown
+
+    // Get all options in the dropdown
+    const options = await page.locator("text[class='placeHolderMainText']").allTextContents();
+    console.log(options)
+    // Iterate through the options and click on "Bengaluru"
+    for (const option of options) {
+        if (option == "Bengaluru"){
+            await page.
+            locator('li').filter({hasText:option}).first().click();
+        }
     }
-})
+});
