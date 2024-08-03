1251c322f89724a73f9c4db16325ff49d84c0776
P S Shrijith <shrijithps89@gmail.com>
2024-08-03 11:22:33 +0530

    playwright

--- pw-practice-app-master/playwright/tests/handlingBootstrapDD.spec.ts
+++ pw-practice-app-master/playwright/tests/handlingBootstrapDD.spec.ts
@@ -1,15 +1,18 @@
-import {test} from '@playwright/test'
+import { test } from '@playwright/test';
 
-test("handlingBootstrapDD", async({page}) => {
-    await page.goto("https://getbootstrap.com/docs/5.2/components/dropdowns/")
-    await page.locator("[data-bs-toggle='dropdown']", {hasText:"Dropdown button"}).first().click();
-    const dropdownElements = await page.locator('.dropdown').locator(".dropdown-menu show").locator('li');
-    console.log(dropdownElements)
-    // for(let dropdownvalue of dropdownElements){
-    //     console.log(dropdownvalue)
-    //     if(dropdownvalue == 'Action'){
-    //         await page.waitForTimeout(2000)
-    //         await page.locator('li').filter({hasText:dropdownvalue}).click()
-    //     }
-    // }
-})
+test('handlingBootstrapDD', async ({ page }) => {
+    await page.goto('https://getbootstrap.com/docs/5.2/components/dropdowns/');
+    
+    await page.locator("[data-bs-toggle='dropdown']", { hasText: "Dropdown button" }).first().click();
+    
+    // Get all dropdown menu items
+    const dropdownItems = await page.locator('.dropdown-menu .dropdown-item').allTextContents();
+    
+    for (let dropdownItem of dropdownItems) {
+        if (dropdownItem.trim() === 'Action') {
+            await page.waitForTimeout(2000);
+            await page.locator('.dropdown-menu .dropdown-item').filter({ hasText: 'Action' }).first().click();
+            break;
+        }
+    }
+});
