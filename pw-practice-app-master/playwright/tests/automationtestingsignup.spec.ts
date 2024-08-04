c46badb2a83ce201772e2f10409ded7958260770
P S Shrijith <shrijithps89@gmail.com>
2024-08-04 08:17:09 +0530

    signing up

--- pw-practice-app-master/playwright/tests/automationtestingsignup.spec.ts
+++ pw-practice-app-master/playwright/tests/automationtestingsignup.spec.ts
@@ -1,22 +1,26 @@
 import {test, expect} from '@playwright/test'
 
 test("automationtestingsignin", async({page}) => {
+
+    let username="testuser89+002";
+    let emailAddress = username+"@test.com";
+
     await page.goto("https://www.automationexercise.com/")
     await page.locator('[alt="Website for automation practice"]').isVisible();
     await page.locator(':text-is("Signup / Login")').click();
     
     await page.locator(':text-is("New User Signup!")').click();
-    await page.getByPlaceholder('Name').fill('testuser89');
-    await page.locator('[type="email"]').fill("testuser89+01@test.com");
-    await page.locator(":text('Signup')").click();
+    await page.getByPlaceholder('Name').fill(username);
+    await page.locator('[data-qa="signup-email"]').fill(emailAddress);
+    await page.locator("button",{hasText:"Signup"}).click();
 
     await page.locator(":text('Enter Account Information')").isVisible();
 
     await page.locator('[data-qa="title"]').locator("#id_gender1").check();
 
     //assertions
-    await expect(page.locator("#name")).toHaveValue('testuser89');
-    await expect(page.locator('[data-qa="email"]')).toHaveValue('testuser89+01@test.com');
+    await expect(page.locator("#name")).toHaveValue(username);
+    await expect(page.locator('[data-qa="email"]')).toHaveValue(emailAddress);
 
     await page.locator("[type='password']").pressSequentially("testuser");
 
@@ -34,7 +38,7 @@ test("automationtestingsignin", async({page}) => {
     await page.locator('[name="first_name"]').fill('test')
 
     //entering last name
-    await page.locator('#last_name').fill("last name")
+    await page.locator('#last_name').fill("lastname")
 
     //entering address
     await page.locator('[data-qa="address"]').fill('address')
@@ -52,6 +56,6 @@ test("automationtestingsignin", async({page}) => {
     await page.locator('#zipcode').fill('560076')
 
     //entering mobile number
-    await page.locator('name="mobile_number"]').fill("987654321")
+    await page.locator('[name="mobile_number"]').fill("987654321")
 
 })
