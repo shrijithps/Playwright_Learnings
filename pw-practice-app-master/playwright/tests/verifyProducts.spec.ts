83536661648bce0691fecafc724fd1d8b32120d9
P S Shrijith <shrijithps89@gmail.com>
2024-08-04 12:53:21 +0530

    view product

--- pw-practice-app-master/playwright/tests/verifyProducts.spec.ts
+++ pw-practice-app-master/playwright/tests/verifyProducts.spec.ts
@@ -3,15 +3,11 @@ import {test} from '@playwright/test'
 test("VerifyProducts", async({page}) => {
     await page.goto("https://www.automationexercise.com")
     await page.locator(':text-is("Products")').click();
-    // await page.locator(":text-is('All Products')").isVisible();
-    // const productContainer = page.locator('.single-products', { hasText: 'Blue Top' });
+    await page.locator(":text-is('All Products')").isVisible();
+    const productContainer = page.locator('.single-products', { hasText: 'Blue Top' });
+    await page.locator('.choose').locator("[href='/product_details/1']").click();
 
-    // const parentContainer = productContainer.locator('..').locator('..').locator('..').locator('..').locator('.choose');
-
-    // // Click on the "View Product" button within that container
-    // await page.locator('a', {hasText:"View Product"}).click();
-
-    const productLocator = page.locator("//h2[contains(text(), 'Blue Top')]/parent::div/parent::div[contains(@class, 'single-products')]");
-    console.log(productLocator.isVisible())
-    await productLocator.locator('.choose').locator("//a[contains(text()='View Product')]").click();
+    // const productLocator = page.locator("//h2[contains(text(), 'Blue Top')]/parent::div/parent::div[contains(@class, 'single-products')]");
+    // console.log(productLocator.isVisible())
+    // await productLocator.locator('.choose').locator("//a[contains(text()='View Product')]").click();
 });
