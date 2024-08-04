4cf93f7f03d20abdbedf7263c9df0f05ab419893
P S Shrijith <shrijithps89@gmail.com>
2024-08-04 14:29:55 +0530

    assertions in product page

--- pw-practice-app-master/playwright/tests/verifyProducts.spec.ts
+++ pw-practice-app-master/playwright/tests/verifyProducts.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
 
 test("VerifyProducts", async({page}) => {
     await page.goto("https://www.automationexercise.com")
@@ -6,6 +6,17 @@ test("VerifyProducts", async({page}) => {
     await page.locator(":text-is('All Products')").isVisible();
     const productContainer = page.locator('.single-products', { hasText: 'Blue Top' });
     await page.locator('.choose').locator("[href='/product_details/1']").click();
+
+    //assertion
+    await expect(page.locator('h2')).toContainText('Blue Top');
+    await expect(page.locator('p')).toContainText('Category: Women > Tops');
+    await expect(page.locator('span')).toContainText('Rs. 500');
+
+    const availability = page.locator('b', {hasText:'Availability'})
+
+    await expect(availability.locator('..').locator('p')).toContainText('In Stock');
+
+    //Verify that detail detail is visible: product name, category, price, availability, condition, brand
 
     // const productLocator = page.locator("//h2[contains(text(), 'Blue Top')]/parent::div/parent::div[contains(@class, 'single-products')]");
     // console.log(productLocator.isVisible())
