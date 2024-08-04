136a4553a8c9ee003fb7519c172bd11857a9bdf1
P S Shrijith <shrijithps89@gmail.com>
2024-08-04 12:48:23 +0530

    product page validation

---
+++ pw-practice-app-master/playwright/tests/verifyProducts.spec.ts
@@ -0,0 +1,17 @@
+import {test} from '@playwright/test'
+
+test("VerifyProducts", async({page}) => {
+    await page.goto("https://www.automationexercise.com")
+    await page.locator(':text-is("Products")').click();
+    // await page.locator(":text-is('All Products')").isVisible();
+    // const productContainer = page.locator('.single-products', { hasText: 'Blue Top' });
+
+    // const parentContainer = productContainer.locator('..').locator('..').locator('..').locator('..').locator('.choose');
+
+    // // Click on the "View Product" button within that container
+    // await page.locator('a', {hasText:"View Product"}).click();
+
+    const productLocator = page.locator("//h2[contains(text(), 'Blue Top')]/parent::div/parent::div[contains(@class, 'single-products')]");
+    console.log(productLocator.isVisible())
+    await productLocator.locator('.choose').locator("//a/i[contains(text()='View Product')]").click();
+});
--- pw-practice-app-master/playwright/tests/verifyProducts.spect.ts
+++
@@ -1,8 +0,0 @@
-import {test} from '@playwright/test'
-
-test("VerifyProducts", async({page}) => {
-    await page.goto("https://www.automationexercise.com")
-    await page.locator(':text-is("Products")').click();
-    await page.locator(":text-is('All Products')").isVisible();
-    
-})
