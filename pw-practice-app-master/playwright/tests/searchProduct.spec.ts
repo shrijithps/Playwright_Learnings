7c5ff53ea9492e5b925d44cc70138bb748756fac
P S Shrijith <shrijithps89@gmail.com>
2024-08-07 18:33:20 +0530

    search product test case

--- pw-practice-app-master/playwright/tests/searchProduct.spec.ts
+++ pw-practice-app-master/playwright/tests/searchProduct.spec.ts
@@ -9,5 +9,15 @@ test("Search product", async({page}) => {
     await page.locator('#submit_search').click();
     await page.waitForTimeout(5000);
     const resulttext = await page.locator('.single-products').locator('[class="productinfo text-center"]').locator('p').allInnerTexts()
-    console.log(resulttext)
+
+    for(let a of resulttext){
+        if(a.includes("Top")){
+            console.log(a)
+        }
+        else{
+            console.error(a, " search results doesnot match the search term")
+            break;
+        }
+    }
+
 })
