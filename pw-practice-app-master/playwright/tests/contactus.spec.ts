26b603b1c11ecd2d90ecf2173e26e9b6c8befaee
P S Shrijith <shrijithps89@gmail.com>
2024-08-08 10:30:25 +0530

    uploading files in playwright

--- pw-practice-app-master/playwright/tests/contactus.spec.ts
+++ pw-practice-app-master/playwright/tests/contactus.spec.ts
@@ -5,8 +5,17 @@ test("contact us", async({page}) => {
     await page.locator("a", {hasText:"Home"}).isVisible()
     await page.locator('a', {hasText:"Contact us"}).click();
     await page.getByPlaceholder('Name').fill("test name")
-    await page.locator("[type='email']").fill("test@test.com")
+    await page.locator('[data-qa="email"]').fill("test@test.com")
     await page.locator("[name='subject']").fill("sample name")
     await page.locator('#message').fill("sample message");
     await page.locator('[name="upload_file"]').setInputFiles('testScreenshots/selectingdate.png');
+    await page.waitForTimeout(2000);
+    await page.locator('[data-qa="submit-button"]').click();
+    page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        await dialog.accept();
+      });
+
+    await page.locator('[class="status alert alert-success"]', {hasText:"Success! Your details have been submitted successfully."});
+
 })
