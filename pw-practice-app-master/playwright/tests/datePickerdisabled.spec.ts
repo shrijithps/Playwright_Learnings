95985fdfc2749f228e398d6b65aa59e074e4a8ca
P S Shrijith <shrijithps89@gmail.com>
2024-08-15 17:53:05 +0530

    datepicker disabled

---
+++ pw-practice-app-master/playwright/tests/datePickerdisabled.spec.ts
@@ -0,0 +1,36 @@
+import {test} from '@playwright/test'
+
+test("playwright datepicker", async({page}) => {
+    let expectedYear = "January 2025";
+    let expectedDate = "25"
+    await page.goto("https://demo.automationtesting.in/Datepicker.html")
+    await page.locator("#datepicker2").click();
+
+    let month = await page.locator("[title='Change the month']").locator('[selected="selected"]').textContent();
+    let year = await page.locator("[title='Change the year']").locator('[selected="selected"]').textContent();
+
+    console.log(month)
+    console.log(year)
+    let yearMonth = month+" "+year
+
+
+    while(!(yearMonth.includes(expectedYear))){
+         await page.locator('.datepick-nav').locator("a", {hasText:"Next"}).click();
+         await page.waitForTimeout(500);
+         month = await page.locator("[title='Change the month']").locator('[selected="selected"]').textContent();
+         year = await page.locator("[title='Change the year']").locator('[selected="selected"]').textContent();
+         yearMonth = month+" "+year
+         if (yearMonth.includes("January") && yearMonth.includes("2025")) {
+             console.log("Found the expected year and month")
+             break;
+         }   
+     }
+
+    // let dates = await page.locator('[data-handler="selectDay"]').locator('a').allTextContents();
+    // for(let date of dates){
+    //     if(date.includes(expectedDate)){
+    //         await page.locator('[data-handler="selectDay"]').locator('a', {hasText:expectedDate}).click();
+    //         break;
+    //     }
+    // }
+})
